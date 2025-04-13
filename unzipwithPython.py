cef1b0049e502550bad1e49cf944c4ca4a75d4d1
Cmcomy 🌞 <awasozozo@gmail.com>
2025-04-13 10:49:56 +0200

    Add files via upload

---
+++ unzipwithPython.py
@@ -0,0 +1,13 @@
+import zipfile
+
+
+zippedFilePath = r"C:\myZipAnimationData\animation.csv.zip"
+unzipPath = r"C:\myUnzipAnimationData"
+
+
+with zipfile.ZipFile(zippedFilePath) as f:
+    ro = f.extractall(path = unzipPath)
+
+with open(rf"{unzipPath}\animation.csv", "r") as f:
+    print(f.readline())
+
